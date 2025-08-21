0b66a75f3d7606465230eeeaf8b699cfada9b76f
sodaeng0051 <104272374+sodaeng0051@users.noreply.github.com>
2025-08-21 21:54:34 +0900

    코드 수정

--- pages/purchase_detail.js
+++ pages/purchase_detail.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { StyleSheet, View, Text, Image } from "react-native";
 import Line10 from "../assets/line-10.svg";
 import Line14 from "../assets/line-14.svg";
+import Line24 from "../assets/line-24.svg";
 import Shareicon from "../assets/share-icon.svg";
 import Hearticon from "../assets/heart-icon.svg";
 import Frame from "../assets/frame.svg";
@@ -65,12 +66,20 @@ const GroupPurchaseDetail = () => {
   return (
     <View style={styles.groupPurchaseDetail}>
       <View style={styles.view}>
+
+        {/* 진행 상태 바 */}
         <View style={[styles.child, styles.itemLayout]} />
         <View style={[styles.item, styles.itemLayout]} />
         <Text style={[styles.text, styles.textTypo1]}>10/15</Text>
         <Text style={[styles.text1, styles.textTypo1]}>70%</Text>
         <Text style={[styles.text2, styles.textTypo1]}>참가한 인원</Text>
-        <Line10 style={styles.inner} width={349} />
+
+        {/* 상단 제품 카드 */}
+        <Image style={[styles.cardProductIcon, styles.iconPosition]} source={card_product} resizeMode="cover" />
+        <Shareicon style={styles.shareIcon} width={22} height={20} />
+        <Hearticon style={[styles.heartIcon, styles.iconLayout1]} width={24} height={24} />
+
+        {/* 남은 시간 / 참여 버튼 */}
         <Line14 style={[styles.lineIcon, styles.iconPosition]} width={350} />
         <View style={[styles.wrapper, styles.wrapperPosition]}>
           <Text style={[styles.text3, styles.textClr]}>홍**</Text>
@@ -81,13 +90,6 @@ const GroupPurchaseDetail = () => {
             <Text style={styles.text6}>남음</Text>
           </Text>
         </View>
-        <Shareicon style={styles.shareIcon} width={22} height={20} />
-        <Image
-            style={[styles.cardProductIcon, styles.iconPosition]}
-            resizeMode="cover"
-            source={card_product}
-          />
-        <Hearticon style={[styles.heartIcon, styles.iconLayout1]} width={24} height={24} />
         <View style={styles.littleRectangle}>
           <View style={styles.childPosition}>
             <View style={[styles.instanceChild, styles.childPosition]} />
@@ -97,30 +99,23 @@ const GroupPurchaseDetail = () => {
           <Text style={[styles.text7, styles.textTypo]}>참여</Text>
         </View>
         <Frame style={[styles.frameIcon, styles.iconLayout]} />
-        <View style={styles.lineView} />
-        <Line26 style={[styles.child1, styles.iconPosition]} width={350} />
-        
+        <Line10 style={styles.inner} width={349} />
+
+        {/* 제품 설명 */}
         <View style={styles.labelParent}>
-            <Text style={styles.descriptionLabel}>제품 설명</Text>
-            <Text style={styles.descriptionText}>
-              탱글한 면발과 촉촉한 미트볼이 어우러진 정통 토마토 스파게티 {"\n"}밀키트
-              입니다. 간편하게 조리해도 레스토랑 퀄리티의 깊은 맛을{"\n"}즐길 수 있어요.
-            </Text>
-          </View>
+          <Text style={styles.descriptionLabel}>제품 설명</Text>
+          <Text style={styles.descriptionText}>
+            탱글한 면발과 촉촉한 미트볼이 어우러진 정통 토마토 스파게티 밀키트{"\n"}
+            입니다. 간편하게 조리해도 레스토랑 퀄리티의 깊은 맛을 즐길 수 있어요.
+          </Text>
+        </View>
+        <Arrowicon2 style={[styles.arrowIcon, styles.arrowIconLayout]} width={8} height={8} />
+        <Line24 style={styles.line24} width={350} />
 
-          <Arrowicon2 
-            style={[styles.arrowIcon, styles.arrowIconLayout]} 
-            width={8} 
-            height={8} 
-          />
-
+        {/* 리뷰 */}
         <View style={[styles.reviewIcon, styles.reviewIconLayout]}>
           <View style={[styles.reviewIconChild, styles.childPosition]} />
-          <Image
-            style={[styles.reviewIconItem, styles.iconLayout]}
-            resizeMode="cover"
-            source={Rectangle}
-          />
+          <Image style={[styles.reviewIconItem, styles.iconLayout]} source={Rectangle} resizeMode="cover" />
         </View>
         <View style={styles.frameParent}>
           <View style={styles.labelGroup}>
@@ -131,15 +126,10 @@ const GroupPurchaseDetail = () => {
             <Text style={[styles.text9, styles.textLayout]}>전체보기</Text>
             <Arrowicon style={styles.seeMoreIcon} width={8} height={8} />
           </View>
-
         </View>
         <View style={[styles.reviewIcon1, styles.reviewIconLayout]}>
           <View style={[styles.reviewIconChild, styles.childPosition]} />
-          <Image
-            style={[styles.reviewIconItem, styles.iconLayout]}
-            resizeMode="cover"
-            source={Rectangle1}
-          />
+          <Image style={[styles.reviewIconItem, styles.iconLayout]} source={Rectangle1} resizeMode="cover" />
         </View>
         <View style={[styles.parent, styles.groupFlexBox]}>
           <Text style={[styles.text10, styles.textTypo1]}>만족해요</Text>
@@ -151,10 +141,14 @@ const GroupPurchaseDetail = () => {
           <Frame2 style={styles.frameIcon1} width={14} height={14} />
           <Text style={styles.text11}>미트볼이 엄청 촉촉!</Text>
         </View>
+
+        {/* 연관 추천 상품 */}
         <View style={[styles.labelWrapper, styles.wrapperFlexBox]}>
           <Text style={[styles.label2, styles.labelTypo]}>연관 추천 상품</Text>
         </View>
-        
+        <Image style={[styles.homeLittleIcon, styles.homeIconLayout]} source={Rectangle2} resizeMode="cover" />
+        <Image style={[styles.homeLittleIcon1, styles.homeIconLayout]} source={Rectangle3} resizeMode="cover" />
+        <Image style={[styles.homeLittleIcon2, styles.homeIconLayout]} source={Rectangle4} resizeMode="cover" />
         <View style={[styles.parent1, styles.parent1Position]}>
           <Text style={[styles.text14, styles.labelTypo]}>미트볼 토마토 스파게티 밀키트</Text>
           <View style={[styles.parent2, styles.textPosition]}>
@@ -163,23 +157,6 @@ const GroupPurchaseDetail = () => {
           </View>
           <Text style={[styles.text17, styles.textPosition]}>12,900원</Text>
         </View>
-        
-        <Image
-          style={[styles.homeLittleIcon, styles.homeIconLayout]}
-          resizeMode="cover"
-          source={Rectangle2}
-        />
-        <Image
-          style={[styles.homeLittleIcon1, styles.homeIconLayout]}
-          resizeMode="cover"
-          source={Rectangle3}
-        />
-        <Image
-          style={[styles.homeLittleIcon2, styles.homeIconLayout]}
-          resizeMode="cover"
-          source={Rectangle4}
-        />
-        
         <View style={[styles.labelContainer, styles.labelParentLayout]}>
           <Text style={[styles.label3, styles.textTypo1]}>
             <Text>
@@ -206,6 +183,7 @@ const GroupPurchaseDetail = () => {
           <Text style={[styles.text8, styles.textTypo1]}>2명 남음</Text>
         </View>
 
+        <Line26 style={[styles.child1, styles.iconPosition]} width={350} />
         <View style={[styles.bigButton, styles.bigPosition]}>
           <View style={styles.childPosition}>
             <View style={[styles.bigButtonChild, styles.childPosition]} />
@@ -220,15 +198,18 @@ const GroupPurchaseDetail = () => {
         <Frame3 style={[styles.frameIcon3, styles.text25Position]} width={24} height={24} />
         <Arrowicon2 style={[styles.arrowIcon2, styles.arrowIconLayout]} width={10} height={10} />
 
+        {/* 하트 아이콘들 */}
         <Hearticon1 style={[styles.heartIcon1, styles.heartIconPosition]} width={18} height={18} />
         <Hearticon2 style={[styles.heartIcon2, styles.heartIconPosition]} width={18} height={18} />
         <Hearticon3 style={[styles.heartIcon3, styles.heartIconPosition]} width={18} height={18} />
+
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+   
   groupPurchaseDetail: {
     backgroundColor: Color.colorGray100,
     flex: 1,
@@ -243,15 +224,17 @@ const styles = StyleSheet.create({
     fontWeight: "600",
     color: Color.colorGray200,
     textAlign: "left",
+    marginLeft: 4,
   },
   descriptionText: {
-    lineHeight: 18,
+    lineHeight: 18, //글씨 높이 조정
     alignSelf: "stretch",
     color: Color.colorDimgray,
     fontSize: 11,
     fontFamily: FontFamily.notoSansKRMedium,
     fontWeight: "500",
     textAlign: "left",
+    marginLeft: 4,
   },
   reviewLabelTypo: {
     textAlign: "left",
@@ -273,7 +256,7 @@ const styles = StyleSheet.create({
     fontWeight: "500",
   },
   iconPosition: {
-    width: 350,
+    width: 349,
     left: 26,
     position: "absolute",
   },
@@ -446,14 +429,15 @@ const styles = StyleSheet.create({
   },
   lineIcon: {
     top: 616,
-    width: 350,
+    width: 349,
     maxHeight: "100%",
+    marginHorizontal: -7,
   },
   text3: {
     fontFamily: FontFamily.notoSansKRMedium,
     fontWeight: "500",
     color: Color.colorDarkslategray100,
-    lineHeight: 24,
+    lineHeight: 25,
     fontSize: FontSize.size_12,
   },
   wrapper: {
@@ -503,7 +487,7 @@ const styles = StyleSheet.create({
   },
   text7: {
     alignSelf: "stretch",
-    fontSize: FontSize.size_12,
+    fontSize: 13,
   },
   frameView: {
     top: 625,
@@ -617,18 +601,19 @@ const styles = StyleSheet.create({
   },
   text10: {
     width: 52,
-    height: 14,
+    height: 15,
     display: "flex",
     alignItems: "center",
     fontSize: FontSize.size_14,
     color: Color.colorDimgray,
-    lineHeight: 14,
+    lineHeight: 14, 
     textAlign: "left",
   },
   frameIcon1: {
     width: 14,
     height: 14,
     overflow: "hidden",
+    marginLeft: -4, // 아이콘 왼쪽으로 이동
   },
   text11: {
     width: 154,
@@ -815,6 +800,14 @@ const styles = StyleSheet.create({
     backgroundColor: Color.colorGray100,
     flex: 1,
   },
+  line24: {
+  top: 760,              // Arrowicon2 바로 밑으로 맞춰 위치
+  left: 20,
+  position: "absolute",
+  width: 350,
+  maxHeight: "100%",
+},
+
 });
 
 export default GroupPurchaseDetail;
